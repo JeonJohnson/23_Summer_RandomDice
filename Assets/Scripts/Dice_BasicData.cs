916eb62456ce1546c11aa3f04d0d6eba99334ebf
Geon0153 <138871242+Geon0153@users.noreply.github.com>
2023-07-20 22:13:04 +0900

    Dice Basic Data/Dice Prefab

--- Assets/Scripts/Dice_BasicData.cs
+++ Assets/Scripts/Dice_BasicData.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Dice_BasicData : MonoBehaviour
 {
+    public float diceDamage;
+    public float attackSpeed;
+    public float criticalProbability;
+    public float criticalDamage;
+
     // Start is called before the first frame update
     void Start()
     {
