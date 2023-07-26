7293e163d1cee7025b2e26d8b0ad3357045fc3b9
JeonJohnson <78492604+JeonJohnson@users.noreply.github.com>
2023-07-26 23:01:20 +0900

    update Enemy alive List Test

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,6 +16,10 @@ public class Spawner : MonoBehaviour {
 
     public float delay = 1f;
 
+    //근희 테스트
+    public List<Enemy> aliveEnemies = new List<Enemy>();
+    //근희 테스트
+
     public void Spawn() {
         if (!isWavePlayed) StartCoroutine("SpawnEnemy");
     }
@@ -25,10 +29,19 @@ public class Spawner : MonoBehaviour {
         isWavePlayed = true;
         while (count < maxCount && isWavePlayed) {
             GameObject enemy = pool.Get(0);
+            
+            
             enemy.transform.position = spawnPoint.position;
             enemy.GetComponent<Enemy>().Init();
             spriter = enemy.GetComponent<SpriteRenderer>();
             spriter.sortingOrder = maxCount - curCount +1;
+
+            //근희 테스트
+            aliveEnemies.Add(enemy.GetComponent<Enemy>());
+            //근희 테스트
+            //적들은, 뒤지거나 혹은 골인지점에 도착하면 aliveEnemis에 접근해서 본인 지우기.
+
+
             yield return new WaitForSeconds(delay);
             curCount += 1;
             count += 1;
