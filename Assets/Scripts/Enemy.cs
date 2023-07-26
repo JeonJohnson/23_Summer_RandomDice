e635ae37b7f55d62f4f19946d7dc393cb245e1cd
Spike-Lee <lsh069283@gmail.com>
2023-07-26 16:29:24 +0900

    Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,17 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Enemy : MonoBehaviour {
+
+    public float speed;
+    public float health;
+    public float maxHealth;
+
+    bool isLive;
+
+    Rigidbody2D rigid;
+
+    public Transform[] wayArray;
+    public int targetWay = 2;
+    Vector2 target;
+
+    void Awake() {
+        rigid = GetComponent<Rigidbody2D>();
+        wayArray = GameObject.Find("Way").GetComponentsInChildren<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        target = wayArray[targetWay].position;
+        Vector2 dirVec = target - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+
+        Vector3 myPos = transform.position;
+        Vector3 targetPos = target;
+        float curDiff = Vector3.Distance(myPos, targetPos);
+        if (curDiff <= 0.1) {
+            switch (targetWay) {
+                case 2:
+                    targetWay = 3;
+                    break;
+                case 3:
+                    targetWay = 4;
+                    break;
+                case 4:
+                    Dead();
+                    break;
+            }
+        }
+    }
+
+    void OnEnable() {
+        isLive = true;
+        rigid.simulated = true;
+        health = maxHealth;
+        targetWay = 2;
+    }
+
+    public void Init() {
+        health = maxHealth;
+        targetWay = 2;
+    }
+
+    void Dead() {
+        gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public PoolManager pool;
+    SpriteRenderer spriter;
+
+    public int curCount;
+    public int maxCount;
+
+    public float delay = 1f;
+
+    public void Spawn() {
+        StartCoroutine("SpawnEnemy");
+    }
+
+    IEnumerator SpawnEnemy() {
+        while (curCount != maxCount) {
+            GameObject enemy = pool.Get(0);
+            enemy.transform.position = spawnPoint.position;
+            enemy.GetComponent<Enemy>().Init();
+            spriter = enemy.GetComponent<SpriteRenderer>();
+            spriter.sortingOrder = maxCount - curCount +1;
+            yield return new WaitForSeconds(delay);
+            curCount += 1;
+        }
+    }
+}
